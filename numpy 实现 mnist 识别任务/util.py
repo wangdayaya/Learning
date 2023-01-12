2379499f88d4a8eb05702ecc0b2378208dade7c1
wangdayaya <21751215@zju.edu.cn>
2023-01-12 17:35:38 +0800

    实现了 softmax_cross_entropy 和 relu 的导数计算方法

--- numpy 实现 mnist 识别任务/util.py
+++ numpy 实现 mnist 识别任务/util.py
@@ -10,6 +10,14 @@ def relu(z):
     return z
 
 
+# derivation_relu 是在求 relu 激活函数的导数
+def derivation_relu(z):
+    flag = (z <= 0)
+    z[flag] = 0
+    z[~flag] = 1
+    return z
+
+
 # softmax 函数就是将一个向量进行转换，将每个值转换到 0-1 之间的一个小数，且经过转换后的向量中的所有小数加起来和为 1
 # z 表示经过线性转换后的结果，大小为 (batch_size, output_size)
 # 返回的结果的大小仍然是  (batch_size, output_size)
@@ -31,3 +39,10 @@ def softmax_cross_entropy(logits, y):
     return a, loss
 
 
+# logits 是经过线性变化的概率结果，大小为 (batch_size, output_size)
+# y 是该批样本的真实标签
+def derivation_softmax_cross_entropy(logits, y):
+    n = logits.shape[0]
+    a = softmax(logits)
+    a[range(n), y] -= 1
+    return a
