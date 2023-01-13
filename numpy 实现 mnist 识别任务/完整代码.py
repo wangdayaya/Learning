162910bc31cca0a01b049f51d41afdba6690545f
wangdayaya <21751215@zju.edu.cn>
2023-01-13 16:00:11 +0800

    完成了 numpy 实现 mnist 任务识别的所有代码，并另外整理合并了一份完整的代码文件

--- numpy 实现 mnist 识别任务/完整代码.py
+++ numpy 实现 mnist 识别任务/完整代码.py
@@ -1,6 +1,83 @@
 from util import *
 import mnistLoader
 
+import numpy as np
+import gzip
+import pickle
+import random
+
+MNIST_FILE_PATH = 'mnist.pkl.gz'
+BATCH_SIZE = 500
+
+
+# 加载 mnist 数据，返回的结果包含了训练集、验证集、测试集三部分数据
+# 训练集有中输入数据大小为 (50000, 784) ，表示有 50000 张图片，每张图片有 784 个像素点，对应有标签大小为 (50000,) ，表示每张图片的所属类别
+# 验证集有中输入数据大小为 (10000, 784) ，表示有 10000 张图片，每张图片有 784 个像素点，对应有标签大小为 (10000,) ，表示每张图片的所属类别
+# 测试集有中输入数据大小为 (10000, 784) ，表示有 10000 张图片，每张图片有 784 个像素点，对应有标签大小为 (10000,) ，表示每张图片的所属类别
+def load_data():
+    with gzip.open(MNIST_FILE_PATH) as f:
+        return pickle.load(f, encoding='bytes')
+
+
+# 先将 data 中的样本和标签分别进行 shuffle ，然后将样本和标签分别切分成每个大小为 batch_size 的批数据
+# BATCH_SIZE 设置为 500 ，最后训练集会有 100 批数据，验证集会有 20 批数据，测试集会有 20 批数据
+def get_batches(data):
+    n = len(data[0])
+    shuffle_data_idx = random.sample(range(n), n)
+    X = data[0][shuffle_data_idx]
+    Y = data[1][shuffle_data_idx]
+    X_batches = [X[i: i + BATCH_SIZE] for i in range(0, n, BATCH_SIZE)]
+    Y_batches = [Y[i: i + BATCH_SIZE] for i in range(0, n, BATCH_SIZE)]
+    return X_batches, Y_batches
+
+
+# relu 激活函数的功能就是将线性转化后的结果中的小于等于 0 的部分都改为 0 ，实现了非线性的转换
+# z 表示经过线性转换后的结果，大小为 (batch_size, hidden_size)
+# 返回的结果的大小仍然是  (batch_size, hidden_size)
+def relu(z):
+    flag = (z <= 0)
+    z[flag] = 0
+    return z
+
+
+# derivation_relu 是在求 relu 激活函数的导数
+def derivation_relu(z):
+    flag = (z <= 0)
+    z[flag] = 0
+    z[~flag] = 1
+    return z
+
+
+# softmax 函数就是将一个向量进行转换，将每个值转换到 0-1 之间的一个小数，且经过转换后的向量中的所有小数加起来和为 1
+# z 表示经过线性转换后的结果，大小为 (batch_size, output_size)
+# 返回的结果的大小仍然是  (batch_size, output_size)
+def softmax(z):
+    max_value_every_batch = np.max(z, axis=-1, keepdims=True)
+    tmp = z - max_value_every_batch
+    return np.exp(tmp) / np.sum(np.exp(tmp), axis=-1, keepdims=True)
+
+
+# 在 softmax 计算结果的基础上，又进行了交叉熵损失的计算
+# logits 表示经过线性变化输出的预测结果，还未经过 softmax 转换，大小为 (batch_size, output_size)
+# y 为该批次的标签，大小为  (batch_size,)
+# 返回的 a 是经过 softmax 转换的结果，loss 是计算出来的交叉熵结果
+def softmax_cross_entropy(logits, y):
+    n = logits.shape[0]
+    a = softmax(logits)
+    scores = a[range(n), y]
+    loss = -np.sum(np.log(scores)) / n
+    return a, loss
+
+
+#  这里计算输出层 softmax_cross_entropy 的误差
+# logits 是经过线性变化的概率结果，大小为 (batch_size, output_size)
+# y 是该批样本的真实标签
+def derivation_softmax_cross_entropy(logits, y):
+    n = logits.shape[0]
+    a = softmax(logits)
+    a[range(n), y] -= 1
+    return a
+
 
 class Network:
     # node_num_list 存放了网络结构中每层神经元个数
